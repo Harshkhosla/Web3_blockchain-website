c507538850d43cd53975cd670a6ef2353ff0784f
harsh <harshkhosla9945@gmail.com>
2022-12-23 13:03:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import { ethers } from "ethers";
+import {abi,contractAddress} from "./constant";
 
 function App() {
+  const [connection ,setConnection]=useState(true)
+ 
+  const ConnectingTometamask=()=>{
+    if(typeof window.ethereum!=="undefined"){
+      window.ethereum.request({method:"eth_requestAccounts"})
+      setConnection(true)
+      
+    }else{
+      console.log("dont see it");
+      setConnection(false)
+    }
+  }
+   async function Fund(){
+    const ethAmount ="77"
+    if(typeof window.ethereum!=="undefined"){
+      console.log(ethers);
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner();
+      console.log(signer);
+      const contract =new ethers.Contract(contractAddress,abi,signer)
+      try{
+
+        const transactionResponse = contract.fund({
+          value:ethers.utils.parseEther(ethAmount)
+        })
+        await listenForTransactionMine(transactionResponse, provider)
+      }catch{
+        console.log("byee");
+      }
+    }
+  }
+  function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}`)
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(
+                    `Completed with ${transactionReceipt.confirmations} confirmations. `
+                )
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" class="btn btn-primary" onClick={ConnectingTometamask}>{connection?"connected":"Connect"}</button>
+      <button type="button" class="btn btn-primary" onClick={Fund}>Fund</button>
     </div>
   );
 }
